3901bc3cddc7852bf3d9d6b8d60fe4943adb9742
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2022-07-05 10:43:02 -0400

    Add files via upload

---
+++ oop_model.py
@@ -0,0 +1,20 @@
+class model:
+    def __init__(self):
+        # properties
+
+        # private methods
+        def __new__():
+            pass
+    
+    # destructor
+    def __del__(self):
+        pass
+
+    # public methods
+    def get_attr(self):
+        pass
+
+    def set_attr(self):
+        pass
+        
+        
